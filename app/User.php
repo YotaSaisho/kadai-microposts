269aaad5da99cfd83118dace102210dd92d94c7b
Yota.S <shoisar8823@gmail.com>
2020-06-05 16:14:01 +0000

    kadai-favorite

--- app/User.php
+++ app/User.php
@@ -58,7 +58,6 @@ class User extends Authenticatable
             return true;
         }
     }
-    
     public function unfollow($userId)
     {
         // 既にフォローしているかの確認
@@ -75,16 +74,41 @@ class User extends Authenticatable
             return false;
         }
     }
-    
     public function is_following($userId)
     {
         return $this->followings()->where('follow_id', $userId)->exists();
     }
-    
     public function feed_microposts()
     {
         $follow_user_ids = $this->followings()->pluck('users.id')->toArray();
         $follow_user_ids[] = $this->id;
         return Micropost::whereIn('user_id', $follow_user_ids);
+    }
+    public function favorites()
+    {
+        return $this->belongsToMany(Micropost::class, 'favorite', 'user_id', 'micropost_id')->withTimestamps();
+    }
+    public function favorite($micropostId)
+    {
+        $exist = $this->is_favorite($micropostId);
+            $this->favorites()->attach($micropostId);
+            return true;
+
+    }
+    public function unfavorite($micropostId)
+    {
+        $exist = $this->is_favorite($micropostId);
+        $its_me = $this->id == $micropostId;
+    
+        if ($exist && !$its_me) {
+            $this->favorites()->detach($micropostId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function is_favorite($micropostId)
+    {
+        return $this->favorites()->where('micropost_id', $micropostId)->exists();
     }
 }
